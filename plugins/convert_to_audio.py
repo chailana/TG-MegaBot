3a7c3b884a1b2135bedacc1fd53213b45380863d
chailana <chailana35217793@gmail.com>
2024-10-12 20:37:22 +0530

    Update convert_to_audio.py

--- plugins/convert_to_audio.py
+++ plugins/convert_to_audio.py
@@ -1,4 +1,3 @@
-
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -24,13 +23,13 @@ from helper_funcs.display_progress import progress_for_pyrogram
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["c2a"]))
 async def convert_to_audio(bot, update):
     TRChatBase(update.from_user.id, update.text, "c2a")
+    
     if str(update.from_user.id) not in Config.SUPER_DLBOT_USERS:
         await bot.send_message(
             chat_id=update.chat.id,
@@ -38,7 +37,8 @@ async def convert_to_audio(bot, update):
             reply_to_message_id=update.id
         )
         return
-    if (update.reply_to_message is not None) and (update.reply_to_message.media is not None) :
+    
+    if (update.reply_to_message is not None) and (update.reply_to_message.media is not None):
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
@@ -47,20 +47,24 @@ async def convert_to_audio(bot, update):
             reply_to_message_id=update.id
         )
         c_time = time.time()
+        
+        # Download the media file
         the_real_download_location = await bot.download_media(
             message=update.reply_to_message,
             file_name=download_location,
             progress=progress_for_pyrogram,
             progress_args=(Translation.DOWNLOAD_START, a.message_id, update.chat.id, c_time)
         )
+
         if the_real_download_location is not None:
-            await bot.edit_message_text(
-                text=Translation.SAVED_RECVD_DOC_FILE,
-                chat_id=update.chat.id,
-                message_id=a.message_id
-            )
-            # don't care about the extension
-            # convert video to audio format
+            if isinstance(a, pyrogram.types.Message):  # Check if 'a' is a Message
+                await bot.edit_message_text(
+                    text=Translation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=a.message_id
+                )
+                
+            # Convert video to audio format
             audio_file_location_path = the_real_download_location
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START,
@@ -68,44 +72,35 @@ async def convert_to_audio(bot, update):
                 message_id=a.message_id
             )
             logger.info(the_real_download_location)
-            # get the correct width, height, and duration for videos greater than 10MB
-            # ref: message from @BotSupport
-            width = 0
-            height = 0
-            duration = 0
+            
+            # Initialize metadata variables
+            width, height, duration = 0, 0, 0
             metadata = extractMetadata(createParser(the_real_download_location))
             if metadata.has("duration"):
                 duration = metadata.get('duration').seconds
+            
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-            if not os.path.exists(thumb_image_path):
-                thumb_image_path = None
-            else:
+            if os.path.exists(thumb_image_path):
                 metadata = extractMetadata(createParser(thumb_image_path))
                 if metadata.has("width"):
                     width = metadata.get("width")
                 if metadata.has("height"):
                     height = metadata.get("height")
-                # get the correct width, height, and duration for videos greater than 10MB
-                # resize image
-                # ref: https://t.me/PyrogramChat/44663
-                # https://stackoverflow.com/a/21669827/4723940
+                # Resize image if it exists
                 Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
                 img = Image.open(thumb_image_path)
-                # https://stackoverflow.com/a/37631799/4723940
-                # img.thumbnail((90, 90))
-                img.resize((90, height))
+                img = img.resize((90, height))  # Resize to thumbnail
                 img.save(thumb_image_path, "JPEG")
-                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            # try to upload file
+            else:
+                thumb_image_path = None
+            
+            # Try to upload file
             c_time = time.time()
             await bot.send_audio(
                 chat_id=update.chat.id,
                 audio=audio_file_location_path,
                 caption=description,
                 duration=duration,
-                # performer="",
-                # title="",
-                # reply_markup=reply_markup,
                 thumb=thumb_image_path,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
@@ -113,12 +108,15 @@ async def convert_to_audio(bot, update):
                     Translation.UPLOAD_START, a.message_id, update.chat.id, c_time
                 )
             )
+            # Clean up temporary files
             try:
-                os.remove(thumb_image_path)
+                if thumb_image_path:
+                    os.remove(thumb_image_path)
                 os.remove(the_real_download_location)
                 os.remove(audio_file_location_path)
-            except:
-                pass
+            except Exception as e:
+                logger.error(f"Error removing files: {e}")
+            
             await bot.edit_message_text(
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
                 chat_id=update.chat.id,
