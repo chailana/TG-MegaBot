dedea56d97cf1197c4c6af8e25daee54239bcb73
chailana <chailana35217793@gmail.com>
2024-10-12 13:58:27 +0530

    Update extract_streams.py

--- plugins/extract_streams.py
+++ plugins/extract_streams.py
@@ -31,7 +31,7 @@ async def extract_sub_title(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NOT_AUTH_USER_TEXT,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
         return
     if update.reply_to_message is not None:
@@ -39,7 +39,7 @@ async def extract_sub_title(bot, update):
         a = await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.DOWNLOAD_START,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
         c_time = time.time()
         the_real_download_location = await bot.download_media(
@@ -67,5 +67,5 @@ async def extract_sub_title(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
